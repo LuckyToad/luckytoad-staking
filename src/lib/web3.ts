442ffdd3542bb67e04b9013847304986749f318e
Riley Gall <rileyg98@gmail.com>
2022-11-04 19:29:04 +1000

    update

--- src/lib/web3.ts
+++ src/lib/web3.ts
@@ -170,14 +170,7 @@ export const checkApprovalOnConnectedWallet = async (amount: number): Promise<bo
 
 	const sig = get(signer);
 	const tokenContract = new ethers.Contract('0xBfB2b6870501a6Ff17121D676A0A45a38c9eeD1e', abi, sig);
-<<<<<<< HEAD
-	const resp: BigNumber = await tokenContract.allowance(await sig.getAddress(), '0x23305ae66432644427fe97C7469E9F06e7D84041');
-
-	return (resp.gte(amt));
-}
-=======
 	const resp = await tokenContract.allowance(await sig.getAddress(), '0x23305ae66432644427fe97C7469E9F06e7D84041');
 	const amtAllowed: BigNumber = resp[0];
 	return amtAllowed.gte(bigNumberAmount);
 };
->>>>>>> fef5c40099fe36ecd1869410992474fbf12aa060

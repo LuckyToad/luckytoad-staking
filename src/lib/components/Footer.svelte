eeb9c5ccef0802059668b5b0b47193c070ce98d6
Jakubdonovan <jakubdonovan@protonmail.com>
2022-11-06 15:52:58 +0700

    added credits in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="flex flex-col justify-center items-center p-8">
 	<div>
 		<p class="text-brand-green-dark text-xs text-center ">© Lucky Toad Team 2022. All right reserved.</p>
-		<!-- <p class="text-brand-green-dark text-xs text-center ">Designed by Nick Jenkins, built by Jakub Donovan.</p> -->
+		<p class="text-brand-green-dark text-xs text-center ">Designed by <a href="https://nickworx.com/" class="underline">Nick Jenkins</a>, built by <a href="https://shieldprotocol.io" class="underline">Jakub Donovan</a>.</p>
 	</div>
 </footer>

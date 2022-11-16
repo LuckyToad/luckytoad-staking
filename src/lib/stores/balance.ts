3c75b1fb7295e163f35ea295561f1c6493dcd4ab
Jakubdonovan <jakubdonovan@protonmail.com>
2022-11-16 11:47:41 +0700

    balance store no longer returns decimals

--- src/lib/stores/balance.ts
+++ src/lib/stores/balance.ts
@@ -2,20 +2,16 @@ import { derived } from 'svelte/store';
 import { ethers } from 'ethers';
 import { provider, signer, signerAddress, contracts } from 'svelte-ethers-store';
 
-export const balance = derived(
-	[signer, provider, contracts, signerAddress],
-	([$signer, $provider, $contracts, $signerAddress], set) => {
-		if (!provider || !$signer || !$provider || !$signerAddress || !$contracts.TOAD)
-			return set(null);
+export const balance = derived([signer, provider, contracts, signerAddress], ([$signer, $provider, $contracts, $signerAddress], set) => {
+	if (!provider || !$signer || !$provider || !$signerAddress || !$contracts.TOAD) return set(null);
 
-		provider.subscribe(async (provider) => {
-			provider.on('block', async () => {
-				const balance = await $contracts.TOAD.balanceOf($signerAddress);
-				const decimals = await $contracts.TOAD.decimals();
-				const formatted = ethers.utils.formatUnits(balance, decimals);
+	provider.subscribe(async (provider) => {
+		provider.on('block', async () => {
+			const balance = await $contracts.TOAD.balanceOf($signerAddress);
+			const decimals = await $contracts.TOAD.decimals();
+			const formatted = Math.trunc(Number(ethers.utils.formatUnits(balance, decimals)));
 
-				set(formatted);
-			});
+			set(formatted);
 		});
-	}
-);
+	});
+});
